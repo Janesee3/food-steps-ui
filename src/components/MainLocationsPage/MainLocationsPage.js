abd78cc00a0050ed5a89c6830ea49b493bfb8dbe
kod2nd <colinongengkiat@gmail.com>
2018-08-02 21:54:14 +0800

    Replaced dummy locations with seedData on MainLocations Page

--- src/components/MainLocationsPage/MainLocationsPage.js
+++ src/components/MainLocationsPage/MainLocationsPage.js
@@ -1,18 +1,14 @@
 import React, { Component } from "react";
 import LocationsList from "../LocationsList/LocationsList";
+import {seedData} from "../UserLocationsPage/seedData"
 
 class MainLocationsPage extends Component {
   constructor() {
     super();
     this.state = {
-      dummylocations: [
-        { lat: 1, lng: 1, placename: "Place 1" },
-        { lat: 2, lng: 2, placename: "Place 2" },
-        { lat: 3, lng: 3, placename: "Place 3" },
-        { lat: 4, lng: 4, placename: "Place 4" }
-      ]
-    };
+      dummylocations: seedData
   }
+}
 
   render() {
     return (
