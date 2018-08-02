8cf0314bd7ff358a91baff6c2a114ace2fef1d49
kod2nd <colinongengkiat@gmail.com>
2018-08-02 10:36:38 +0800

    changed locations.js to class component

--- src/components/location/Location.js
+++ src/components/location/Location.js
@@ -1,11 +1,33 @@
-import React from 'react';
+// import React from 'react';
 
-const LocationComponent = () => {
-    return (
-        <div>
-            Location here!
-        </div>
-    );
+// const LocationComponent = () => {
+//     return (
+//         <div>
+//             Location here!
+//         </div>
+//     );
+// }
+
+// export default LocationComponent;
+
+import React, { Component } from 'react';
+
+class LocationComponent extends Component {
+    constructor(){
+        super()
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                Hello from Locations Router
+            </div>
+        );
+    }
 }
 
 export default LocationComponent;
+
