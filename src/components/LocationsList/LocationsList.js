e1b446d3a5aab20546825a0a984ae02c9a6bddb7
kod2nd <colinongengkiat@gmail.com>
2018-08-02 14:07:57 +0800

    added locations-list route

--- src/components/LocationsList/LocationsList.js
+++ src/components/LocationsList/LocationsList.js
@@ -1,9 +1,9 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
 import 'antd/dist/antd.css';
-import './index.css';
+import '../../index.css';
 import { List, Avatar, Icon, Rate } from 'antd';
+
 
 const listData = [];
 for (let i = 1; i < 23; i++) {
@@ -23,34 +23,51 @@ const IconText = ({ type, text }) => (
   </span>
 );
 
-ReactDOM.render(
-  
-  <List
-    itemLayout="vertical"
-    size="small"
-    pagination={{
-      onChange: (page) => {
-        console.log(page);
-      },
-      pageSize: 2,
-    }}
-    dataSource={listData}
-    footer={<div><b>ant design</b> footer part</div>}
-    renderItem={item => (
-      <List.Item
-        actions={[<a>edit</a>, <a>more</a>]}
-        extra={<img width={200} alt="logo" src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png" />}
-      >
-        <List.Item.Meta
-          avatar={<Avatar src={item.avatar} />}
-          title={<a href={item.href}>{item.title}</a>}
-          description={item.description}
+class LocationsList extends Component {
+  render() {
+    return (
+      <div className="locations-list">
+        <List
+          itemLayout="vertical"
+          size="small"
+          pagination={{
+            onChange: (page) => {
+              console.log(page);
+            },
+            pageSize: 5,
+          }}
+          dataSource={listData}
+          footer={<div><b>ant design</b> footer part</div>}
+          renderItem={item => (
+            <List.Item
+              actions={[<a>edit</a>, <a>more</a>]}
+              extra={<img width={200} alt="logo" src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png" />}
+            >
+              <List.Item.Meta
+                avatar={<Avatar src={item.avatar} />}
+                title={<a href={item.href}>{item.title}</a>}
+                description={item.description}
+              />
+              {item.content}
+              <div>
+                <Rate allowHalf defaultValue={2.5} />
+              </div>
+            </List.Item>
+          )}
         />
-        {item.content}
-        <div>
-        <Rate allowHalf defaultValue={2.5} />
-        </div>
-      </List.Item>
-    )}
-  />,
-  document.getElementById('container'));
+      </div>
+    );
+  }
+}
+
+export default LocationsList;
+
+
+
+
+
+
+
+
+
+
--- src/components/NavRoutes/NavRoutes.js
+++ src/components/NavRoutes/NavRoutes.js
@@ -3,13 +3,19 @@ import { Route } from "react-router-dom";
 import MainLocationsPage from "../MainLocationsPage/MainLocationsPage";
 import { Layout } from "antd";
 import './NavRoutes.css'
+import LocationsList from '../LocationsList/LocationsList'
 const { Content } = Layout;
+
+
+
 
 const NavRoutes = () => {
   return (
     <Content className="content-container">
       <Route path="/location" component={MainLocationsPage} />
+      <Route path="/location-list" component={LocationsList} />
     </Content>
+
   );
 };
 
