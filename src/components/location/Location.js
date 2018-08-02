11a66a3b6906895b8af428462d7d33c419c7d197
kod2nd <colinongengkiat@gmail.com>
2018-08-02 10:48:54 +0800

    Added dummy locations to state for testing

--- src/components/location/Location.js
+++ src/components/location/Location.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import AllLocations from './AllLocations';
 
 class LocationComponent extends Component {
-    constructor(){
+    constructor() {
         super()
         this.state = {
-
+            dummylocations: [{ lat: 1, lng: 1 }, { lat: 2, lng: 2 }, { lat: 3, lng: 3 }, { lat: 4, lng: 4 }]
         }
     }
 
@@ -12,6 +13,7 @@ class LocationComponent extends Component {
         return (
             <div>
                 Hello from Locations Router
+                <AllLocations />
             </div>
         );
     }
