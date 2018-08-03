2cf19abaa80424eb504d9101cdbc786cfb81b9be
kod2nd <colinongengkiat@gmail.com>
2018-08-03 10:19:52 +0800

    Fetched data for UserLocations List from food-steps-api

--- src/components/UserLocationsPage/UserLocationsPage.js
+++ src/components/UserLocationsPage/UserLocationsPage.js
@@ -2,29 +2,29 @@ import React, { Component } from 'react';
 import LocationsLists from '../LocationsList/LocationsList'
 import { seedData } from './seedData'
 
-// const URL = "https://localhost:3000/locations/user/5b62eb31c0f5b0551cced58f"
+const URL = "http://localhost:3000/locations/user/5b62eb31c0f5b0551cced58f"
 // const URL2 = "https://randomuser.me/api/?results=5&inc=name,gender,email,nat&noinfo"
 
 class UserLocationsPage extends Component {
     constructor() {
         super()
         this.state = {
-            userLocations: [{ description: "Description", jane: "Title", sahil: "content" }, { description: "Description", jane: "Title", sahil: "content" }]
+            userLocations: []
         }
     }
 
     async componentDidMount() {
-        // const response = await fetch(URL);
-        // const userLocationData = await response.json()
-        // console.log("UserLocations Data", userLocationData)
+        const response = await fetch(URL);
+        const userLocationData = await response.json()
+        console.log("UserLocations Data", userLocationData)
         this.setState({
-            userLocations: seedData
+            userLocations: userLocationData
         })
     }
 
 
     render() {
-        console.log("saifhaif", this.state.userLocations)
+        // console.log("saifhaif", this.state.userLocations)
         return (
             <div>
                 <LocationsLists userLocations={this.state.userLocations} detailed={true} />
