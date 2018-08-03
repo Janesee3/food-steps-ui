6795b8befce2b88e00ffa73a29cb3671095b6f09
MayuriR29 <mayuri.gr@gmail.com>
2018-08-03 13:51:45 +0800

    add networkUtils file with postToServer method

---
+++ src/utils/networkUtils.js
@@ -0,0 +1,16 @@
+export const API_HOST = process.env.REACT_API_HOST || "http://localhost:3000";
+
+export const postToServer = async (url, body, includeCredentials) => {
+  const credentials = includeCredentials ? "include" : "omit";
+
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+    },
+    body: JSON.stringify(body),
+    credentials: credentials
+  });
+
+  return res;
+};
