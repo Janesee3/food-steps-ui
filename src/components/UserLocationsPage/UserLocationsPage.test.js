1638b7e934a2146477560d7b2e587bde839d0acd
kod2nd <colinongengkiat@gmail.com>
2018-08-06 14:07:57 +0800

    refactored userlocationspage.test

--- src/components/UserLocationsPage/UserLocationsPage.test.js
+++ src/components/UserLocationsPage/UserLocationsPage.test.js
@@ -5,19 +5,12 @@ import { API_HOST } from "../../utils/networkUtils";
 
 const fetchMockData = async () => {
   fetch.mockResponseOnce(JSON.stringify(["data1", "data2"]));
-
   const renderer = new ShallowRenderer();
   renderer.render(<UserLocationsPage />);
   const instance = renderer.getMountedInstance();
   await instance.componentDidMount();
-  const result = renderer.getRenderOutput();
-
-  return result;
+  return renderer.getRenderOutput();
 };
-
-test("smoke test ", () => {
-  expect(1).toBe(1);
-});
 
 beforeEach(() => {
   fetch.resetMocks();
@@ -31,7 +24,7 @@ test("userLocation component didMount calls for a fetch and passed fetched as pr
   });
 });
 
-test("Snapshot fetch should match mocked data", async() => {
+test("Snapshot fetch should match mocked data", async () => {
   const result = await fetchMockData();
-  expect(result).toMatchSnapshot(); 
+  expect(result).toMatchSnapshot();
 });
