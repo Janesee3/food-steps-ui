a7c81fa4c244d8f40b3807d81225562cdd3ff82e
yuhanyuhan <yuhanlian@gmail.com>
2018-08-07 16:53:36 +0800

    Added Cypress for Signin and signup

---
+++ cypress/integration/SignIn.spec.js
@@ -0,0 +1,42 @@
+context("Access signin/signup", () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:3001");
+    });
+
+    it('signin success', () => {
+        cy.get('.sign-up-button').click()  
+        cy.contains('.ant-modal','Welcome To Food Steps')
+
+        cy.get('input#username.ant-input')
+        .type('user11')
+        cy.get('input#password.ant-input')
+        .type('12345678')
+
+        cy
+        .get('.ant-modal-content')
+        .contains('button','Sign In')
+        .click({force:true}) 
+
+        cy.wait(500)
+        cy.contains('div.ant-message-success', 'Successfully signed in! Welcome user11!')
+    })  
+    
+    it('signin unsuccessful', () => {
+      cy.get('.sign-up-button').click()  
+      cy.contains('.ant-modal','Welcome To Food Steps')
+
+      cy.get('input#username.ant-input')
+      .type('user111')
+      cy.get('input#password.ant-input')
+      .type('12345678')
+
+      cy
+      .get('.ant-modal-content')
+      .contains('button','Sign In')
+      .click({force:true}) 
+
+      cy.wait(500)
+      cy.contains('div.ant-message-notice', 'Username or password is incorrect.')
+  })  
+  });
+  
