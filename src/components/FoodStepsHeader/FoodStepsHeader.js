29c61e89aa8df9562f37b5fe9edb7cc91041b217
MayuriR29 <mayuri.gr@gmail.com>
2018-08-07 09:36:24 +0800

    Added Avatar to header

--- src/components/FoodStepsHeader/FoodStepsHeader.js
+++ src/components/FoodStepsHeader/FoodStepsHeader.js
@@ -2,96 +2,106 @@ import { Layout } from "antd";
 import NavBar from "../NavBar/NavBar";
 import SignInSignUpModal from "../SignInSignUpModal/SignInSignUpModal";
 import React, { Component } from "react";
-import { Button, message } from "antd";
+import { Button, message, Avatar } from "antd";
 import "./FoodStepsHeader.css";
 
 const { Header } = Layout;
 class FoodStepsHeader extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			visible: false
-		};
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalVisible: false,
+      isButtonVisible: true,
+      displayName: ""
+    };
 
-		this.closeModal = this.closeModal.bind(this);
-		this.onSignUpSuccess = this.onSignUpSuccess.bind(this);
-		this.onSignUpFail = this.onSignUpFail.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.onSignUpSuccess = this.onSignUpSuccess.bind(this);
+    this.onSignUpFail = this.onSignUpFail.bind(this);
 
-		this.onSignInSuccess = this.onSignInSuccess.bind(this);
-		this.onSignInFail = this.onSignInFail.bind(this);
-	}
+    this.onSignInSuccess = this.onSignInSuccess.bind(this);
+    this.onSignInFail = this.onSignInFail.bind(this);
+  }
 
-	// Sign In Sign Up Modal Callbacks
+  // Sign In Sign Up Modal Callbacks
 
-	onSignUpSuccess = username => {
-		this.setState({ visible: false });
-		this.props.onSignInAppCallback();
-		message.success(`Successfully created account! Welcome ${username}!`, 3);
-	};
+  onSignUpSuccess = username => {
+    this.setState({
+      isModalVisible: false,
+      isButtonVisible: false,
+      displayName: username
+    });
+    message.success(`Successfully created account! Welcome ${username}!`, 3);
+  };
 
-	onSignUpFail = errMessage => {
-		let displayMessage = `Sign up failed due to unexpected error, please contact admin!`;
+  onSignUpFail = () => {
+    message.error(
+      `Sign up failed due to unexpected error, please contact admin!`,
+      3
+    );
+  };
 
-		if (errMessage && errMessage.includes("unique")) {
-			displayMessage =
-				"This username is already used! Please choose another username.";
-		}
+  onSignInSuccess = username => {
+    this.setState({ isModalVisible: false, isButtonVisible: false });
+    this.displayName = username;
+    message.success(`Successfully signed in! Welcome ${username}!`, 3);
+  };
 
-		message.error(displayMessage, 3);
-	};
+  onSignInFail = () => {
+    message.error(
+      `Sign in failed due to unexpected error, please contact admin!`,
+      3
+    );
+  };
 
-	onSignInSuccess = username => {
-		this.setState({ visible: false });
-		this.props.onSignInAppCallback();
-		message.success(`Successfully signed in! Welcome ${username}!`, 3);
-	};
+  closeModal = () => {
+    this.setState({ isModalVisible: false });
+  };
 
-	onSignInFail = errMessage => {
-		let displayMessage = `Sign in failed due to unexpected error, please contact admin!`;
+  showModal = () => {
+    this.setState({
+      isModalVisible: true
+    });
+  };
 
-		if (
-			errMessage &&
-			(errMessage.includes("match") || errMessage.includes("found"))
-		) {
-			displayMessage = "Username or password is incorrect.";
-		}
+  render() {
+    const loginButton = (
+      <Button
+        className="sign-up-button"
+        type="primary"
+        onClick={this.showModal}
+      >
+        Sign Up / Sign In
+      </Button>
+    );
 
-		message.error(displayMessage, 3);
-	};
+    const userAvatar = (
+      <div className="avatar">
+        <Avatar icon="user" />
+        {this.state.displayName}
+      </div>
+    );
 
-	closeModal = () => {
-		this.setState({ visible: false });
-	};
-
-	showModal = () => {
-		this.setState({ visible: true });
-	};
-
-	render() {
-		return (
-			<div>
-				<Header>
-					<div className="logo" />
-					<Button
-						className="sign-up-button"
-						type="primary"
-						onClick={this.showModal}
-					>
-						Sign Up / Sign In
-					</Button>
-					<SignInSignUpModal
-						isModalVisible={this.state.visible}
-						handleCancel={this.closeModal}
-						onSignUpSuccess={this.onSignUpSuccess}
-						onSignUpFail={this.onSignUpFail}
-						onSignInSuccess={this.onSignInSuccess}
-						onSignInFail={this.onSignInFail}
-					/>
-					<NavBar />
-				</Header>
-			</div>
-		);
-	}
+    return (
+      <div>
+        <Header>
+          <div className="logo" />
+          {this.state.isButtonVisible ? loginButton : userAvatar}
+          <SignInSignUpModal
+            isModalVisible={this.state.isModalVisible}
+            handleCancel={this.closeModal}
+            onSignUpSuccess={this.onSignUpSuccess}
+            onSignUpFail={this.onSignUpFail}
+            onSignInSuccess={this.onSignInSuccess}
+            onSignInFail={this.onSignInFail}
+          />
+          <NavBar />
+        </Header>
+      </div>
+    );
+  }
 }
+
+
 
 export default FoodStepsHeader;
