e686668376b0a120758b3c5df5142e5e7aa958fe
kod2nd <colinongengkiat@gmail.com>
2018-08-10 18:34:57 +0800

    Updated Nav Routes

--- src/components/NavRoutes/NavRoutes.js
+++ src/components/NavRoutes/NavRoutes.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import MainLocationsPage from "../MainLocationsPage/MainLocationsPage";
-import LocationFormContainer from "../LocationFormContainer/LocationFormContainer";
 import { Layout } from "antd";
 import "./NavRoutes.css";
 import UserLocationsPage from "../UserLocationsPage/UserLocationsPage";
-import LocationsList from "../LocationsList/LocationsList";
 import "./NavRoutes.css";
 const { Content } = Layout;
 
 const NavRoutes = props => {
 	return (
 		<Content className="content-container">
-			<Route path="/" exact component={MainLocationsPage} />
+			<Route path="/" exact render={() => <MainLocationsPage isLoggedInUser={props.isLoggedInUser}/>}/>
 			<Route path="/user-locations-page" component={UserLocationsPage} />
 			{/* <Route path="/location-list" component={LocationsList} /> */}
 			{/* <Route path="/new-location" render={() => <LocationFormContainer isLoggedInUser={props.isLoggedInUser} />} /> */}
