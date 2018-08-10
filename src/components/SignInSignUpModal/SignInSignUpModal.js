a95a3bf831b609a5e15ee1cd625494484de0c7ac
See Loo Jane <muigek34@yahoo.com.sg>
2018-08-10 10:21:59 +0800

    Removed unused callback on tab change

--- src/components/SignInSignUpModal/SignInSignUpModal.js
+++ src/components/SignInSignUpModal/SignInSignUpModal.js
@@ -5,10 +5,6 @@ import SignInForm from "../SignInForm/SignInForm";
 
 const TabPane = Tabs.TabPane;
 
-function callback(key) {
-	// console.log(key);
-}
-
 class SignInSignUpModal extends Component {
 	onSignUpSuccess = username => {
 		this.props.closeModal();
@@ -55,7 +51,7 @@ class SignInSignUpModal extends Component {
 					onCancel={this.props.closeModal}
 					footer={[]}
 				>
-					<Tabs defaultActiveKey="1" onChange={callback}>
+					<Tabs defaultActiveKey="1">
 						<TabPane tab="Sign In" key="1">
 							<SignInForm
 								onSignInSuccess={this.onSignInSuccess}
