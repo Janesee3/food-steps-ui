7f41119c33695694e544e518c1c42b75d41455ae
See Loo Jane <muigek34@yahoo.com.sg>
2018-08-10 10:00:03 +0800

    Make cypress testcase wait for 10 secs beforeAll to wake dyno up

--- cypress/integration/01_signup.spec.js
+++ cypress/integration/01_signup.spec.js
@@ -1,51 +1,49 @@
 context("Access signup", () => {
-    beforeEach(() => {
-      cy.visit('');
+	beforeAll(() => {
+		cy.visit("");
+		cy.wait(10000);
+	});
 
-      cy.get('.sign-up-button').click()
-      cy.contains('.ant-modal','Welcome To Food Steps')
+	beforeEach(() => {
+		cy.visit("");
 
-      cy.contains('.ant-tabs-tab', "Sign Up").click()
-    });
+		cy.get(".sign-up-button").click();
+		cy.contains(".ant-modal", "Welcome To Food Steps");
 
-    it('signup success', () => {
-        cy.get('#signup-form input#username.ant-input')
-        .type('user11')
-        cy.get('#signup-form input#password.ant-input')
-        .type('12345678')
-        cy.get('#signup-form input#confirm.ant-input')
-        .type('12345678')
-        cy.get('#signup-form input#email.ant-input')
-        .type('abc@abc.com')
+		cy.contains(".ant-tabs-tab", "Sign Up").click();
+	});
 
-        cy
-        .get('.ant-modal-content')
-        .contains('button','Register')
-        .click({force:true}) 
+	it("signup success", () => {
+		cy.get("#signup-form input#username.ant-input").type("user11");
+		cy.get("#signup-form input#password.ant-input").type("12345678");
+		cy.get("#signup-form input#confirm.ant-input").type("12345678");
+		cy.get("#signup-form input#email.ant-input").type("abc@abc.com");
 
-        cy.wait(500)
-        cy.contains('div.ant-message-success', `Successfully created account! Welcome user11!`)
-    })  
+		cy.get(".ant-modal-content")
+			.contains("button", "Register")
+			.click({ force: true });
 
+		cy.wait(500);
+		cy.contains(
+			"div.ant-message-success",
+			`Successfully created account! Welcome user11!`
+		);
+	});
 
-    it('signup user name existed', () => {
-        cy.get('#signup-form input#username.ant-input')
-        .type('user11')
-        cy.get('#signup-form input#password.ant-input')
-        .type('12345678')
-        cy.get('#signup-form input#confirm.ant-input')
-        .type('12345678')
-        cy.get('#signup-form input#email.ant-input')
-        .type('abc@abc.com')
+	it("signup user name existed", () => {
+		cy.get("#signup-form input#username.ant-input").type("user11");
+		cy.get("#signup-form input#password.ant-input").type("12345678");
+		cy.get("#signup-form input#confirm.ant-input").type("12345678");
+		cy.get("#signup-form input#email.ant-input").type("abc@abc.com");
 
-        cy
-        .get('.ant-modal-content')
-        .contains('button','Register')
-        .click({force:true}) 
+		cy.get(".ant-modal-content")
+			.contains("button", "Register")
+			.click({ force: true });
 
-        cy.wait(500)
-        cy.contains('div.ant-message-notice', `This username is already used! Please choose another username.`)
-    })  
- 
-  });
-  
+		cy.wait(500);
+		cy.contains(
+			"div.ant-message-notice",
+			`This username is already used! Please choose another username.`
+		);
+	});
+});
