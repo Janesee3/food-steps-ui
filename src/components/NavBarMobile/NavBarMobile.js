7e9dc6b91c3edbf26f5fbabedc2dba4d44fe97c4
ngyl88 <ng_yl88@hotmail.com>
2018-08-15 16:44:35 +0800

    update header styling (wip)

--- src/components/NavBarMobile/NavBarMobile.js
+++ src/components/NavBarMobile/NavBarMobile.js
@@ -5,9 +5,11 @@ import { menu } from "../NavBar/NavBar";
 const NavBarMobile = () => {
   return (
     <Dropdown overlay={menu} trigger={["click"]}>
+    <div>
       <Button type="primary">
         Menu <Icon type="down" />
       </Button>
+    </div>
     </Dropdown>
   );
 };
