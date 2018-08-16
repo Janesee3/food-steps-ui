7292fe888c2769cf1c8b309853a4612405292165
See Loo Jane <muigek34@yahoo.com.sg>
2018-08-16 10:56:05 +0800

    remove console.logs

--- src/components/SimpleUserLocation/SimpleUserLocation.js
+++ src/components/SimpleUserLocation/SimpleUserLocation.js
@@ -2,32 +2,30 @@ import React from "react";
 import { List, Avatar, Icon } from "antd";
 
 const IconText = ({ type, text }) => (
-  <span>
-    <Icon type={type} style={{ marginRight: 8 }} />
-    {text}
-  </span>
+	<span>
+		<Icon type={type} style={{ marginRight: 8 }} />
+		{text}
+	</span>
 );
 
 const SimpleUserLocation = props => {
-  const location = props.location;
-  return (
-    <List.Item onClick={()=>console.log('List clicked')}>
-      <List.Item.Meta
-        avatar={<Avatar src={location.avatar} />}
-        title={
-          <a href={location.href}>
-            {location.locationName} {` | `}
-            <IconText type="star" text={location.rating} />
-          </a>
-        }
-        description={
-          location.globalLocation.geocodedLocationName
-        }
-      />
+	const location = props.location;
+	return (
+		<List.Item>
+			<List.Item.Meta
+				avatar={<Avatar src={location.avatar} />}
+				title={
+					<a href={location.href}>
+						{location.locationName} {` | `}
+						<IconText type="star" text={location.rating} />
+					</a>
+				}
+				description={location.globalLocation.geocodedLocationName}
+			/>
 
-      {/* Insert Content here */}
-    </List.Item>
-  );
+			{/* Insert Content here */}
+		</List.Item>
+	);
 };
 
 export default SimpleUserLocation;
