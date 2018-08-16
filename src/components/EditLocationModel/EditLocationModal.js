77ca5722dd02817a0efdda74de7bc5af91b59a24
ngyl88 <ng_yl88@hotmail.com>
2018-08-16 23:39:37 +0800

    #39 reuse LocationForm for edit, use wrappedComponentRef for validation

--- src/components/EditLocationModel/EditLocationModal.js
+++ src/components/EditLocationModel/EditLocationModal.js
@@ -2,33 +2,52 @@ import React from "react";
 import { Modal, Form, Input, Card } from "antd";
 import LocationForm from "../LocationForm/LocationForm";
 
+const isDevelopment = process.env.NODE_ENV === "development";
+
 class EditLocationModal extends React.Component {
   constructor() {
     super();
     this.state = {
-      updatedLocationName: undefined
+      updatedLocationName: undefined,
+      formRef: undefined
     };
   }
+
   handleFormChange = event => {
     this.setState({
       updatedLocationName: event.target.value
     });
   };
 
+  saveFormRef = (formRef) => {
+    this.formRef = formRef;
+  }
+
+  onUpdateClicked = () => {
+    this.formRef.props.form.validateFieldsAndScroll(this.onValidationCompletion);
+  }
+
+  onValidationCompletion = async (err, values) => {
+    if (err) return isDevelopment && console.error(err);
+
+    this.props.onUpdate(this.state.updatedLocationName);
+  };
+
   render() {
-    const { visible, closeModal, onUpdate, location } = this.props;
+    const { location } = this.props;
 
     return (
       <Modal
-        visible={visible}
+        visible={this.props.visible}
         title="Edit Food Location"
         okText="Update"
-        onCancel={closeModal}
-        onOk={()=>onUpdate(this.state.updatedLocationName)} 
+        onCancel={this.props.closeModal}
+        onOk={this.onUpdateClicked} 
       >
         {location && (
           <LocationForm
             editMode={true}
+            wrappedComponentRef={this.saveFormRef}
             selectedLocation={location}
             onLocationNameInputChange={this.handleFormChange}
             locationNameInput={location.locationName}
