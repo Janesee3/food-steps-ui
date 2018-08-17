a7dd97def8d2a5d268b2c34c61706cb58cb76ece
See Loo Jane <muigek34@yahoo.com.sg>
2018-08-17 13:57:29 +0800

    remove placeholder text for ratings in simpleUSerLocation

--- src/components/SimpleUserLocation/SimpleUserLocation.js
+++ src/components/SimpleUserLocation/SimpleUserLocation.js
@@ -2,31 +2,31 @@ import React from "react";
 import { List, Avatar, Icon } from "antd";
 
 const IconText = ({ type, text }) => (
-  <span>
-    <Icon type={type} style={{ marginRight: 8 }} />
-    {text}
-  </span>
+	<span>
+		<Icon type={type} style={{ marginRight: 8 }} />
+		{text}
+	</span>
 );
 
 const SimpleUserLocation = props => {
-  const location = props.location;
-  return (
-    <List.Item>
-      <List.Item.Meta
-        avatar={<Avatar src={location.avatar} />}
-        title={
-          <span>
-            {location.locationName}
-            {` | `}
-            <IconText type="star" text={location.rating || 3.0} />
-          </span>
-        }
-        description={location.globalLocation.geocodedLocationName}
-      />
+	const location = props.location;
+	return (
+		<List.Item>
+			<List.Item.Meta
+				avatar={<Avatar src={location.avatar} />}
+				title={
+					<span>
+						{location.locationName}
+						{/* {` | `}
+            <IconText type="star" text={location.rating || 3.0} /> */}
+					</span>
+				}
+				description={location.globalLocation.geocodedLocationName}
+			/>
 
-      {/* Insert Content here */}
-    </List.Item>
-  );
+			{/* Insert Content here */}
+		</List.Item>
+	);
 };
 
 export default SimpleUserLocation;
